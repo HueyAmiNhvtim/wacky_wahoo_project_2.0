119bbac400ef8d04b01caf365216d27f6ce53d62
HueyAmiNhvtim <dshk127mm@gmail.com>
2026-05-08 18:14:08 -0400

    Create preliminary comment extractor class

---
+++ scraper.py
@@ -0,0 +1,18 @@
+# Probably store the credential stuff into a .env file.
+
+class Scraper:
+    def __init__(self):
+        pass
+
+    def extract_comments(self):
+        self._extract_comment_section()
+        self._extract_livechat()
+
+    def _extract_comment_section(self):
+        pass
+
+    def _extract_livechat(self):
+        pass
+
+    def extract_video_info(self):
+        pass
