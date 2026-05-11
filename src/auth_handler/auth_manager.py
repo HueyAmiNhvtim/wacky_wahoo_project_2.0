9ff1c770088c51e61276400efc0ddf281cc9fe8d
HueyAmiNhvtim <dshk127mm@gmail.com>
2026-05-10 23:55:51 -0400

    Initial authentication handler for video streaming service

---
+++ src/auth_handler/auth_manager.py
@@ -0,0 +1,58 @@
+import os
+import googleapiclient.discovery
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.errors import HttpError
+
+class AuthManager:
+    """Centralized Authentication Manager for all platforms."""
+    
+    def __init__(self):
+        self.youtube_client = None
+        self.twitch_client = None
+        
+        self.__read_env()
+        self.authenticate_all()
+    
+    def __read_env(self):
+        """Read and initialize the environment variables"""
+        self.ytb_name = os.getenv("YTB_API_SERVICE_NAME", default="youtube")
+        self.ytb_version = os.getenv("YTB_API_VERSION", default="v3")
+        self.ytb_scopes = os.getenv("YTB_API_SCOPES", default="").split(",")
+        self.ytb_token_fp = os.getenv("YTB_TOKEN_FP", default="")
+        self.ytb_creds_fp = os.getenv("YTB_CRED_FP", default="")
+    
+    def authenticate_youtube(self):
+        """Authenticate and return the YouTube API client."""
+        creds = None
+        if os.path.exists(self.ytb_token_fp):
+            creds = Credentials.from_authorized_user_file(self.ytb_token_fp,  self.ytb_scopes)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    self.ytb_creds_fp, self.ytb_scopes
+                )
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open(self.ytb_token_fp, "w") as token:
+                token.write(creds.to_json())
+        
+        self.youtube_client  = googleapiclient.discovery.build(self.ytb_name, self.ytb_version, credentials=creds)
+        return self.youtube_client
+        
+    def authenticate_twitch(self):
+        """Authenticate and return the Twitch API client."""
+        if not self.twitch_client:
+            # TODO: Implement Twitch OAuth logic here. Yeah, probably for the future tbh.
+            self.twitch_client = "twitch_authenticated_client_placeholder"
+        return self.twitch_client
+
+    def authenticate_all(self):
+        """Authenticate all available platforms in one go."""
+        self.authenticate_youtube()
+        # self.authenticate_twitch()
+        return self
