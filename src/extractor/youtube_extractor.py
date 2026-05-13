7f986e23badaa9defa9117fd6bbe96bd794163bf
HueyAmiNhvtim <dshk127mm@gmail.com>
2026-05-12 22:23:44 -0400

    First version of youtube_extractor's extract_comments

--- src/extractor/youtube_extractor.py
+++ src/extractor/youtube_extractor.py
@@ -1,16 +1,72 @@
 # Probably store the credential stuff into a .env file.
 # Also probably a main class to log into OAuth stuff.
+from googleapiclient.errors import HttpError
 from base import BaseExtractor
 from typing import List
 
 
 class YoutubeExtractor(BaseExtractor):
-    def __init__(self):
-        pass
+    def __init__(self, youtube_client):
+        self.youtube_client = youtube_client
 
     def extract_comments(self, video_id: str) -> List[str]:
         """Extract post-stream comments."""
         result = []
+        try:
+            request = self.youtube_client.commentThreads().list(
+                part="snippet,replies",
+                maxResults=1,
+                moderationStatus="published",
+                textFormat="html",
+                videoId="fAcT-LezxAY"
+            )
+            response = request.execute()
+            # nextPageToken = response.get("nextPageToken", None)
+            # while nextPageToken:
+            request = self.youtube_client.commentThreads().list(
+                part="snippet,replies",
+                maxResults=1,
+                moderationStatus="published",
+                textFormat="html",
+                videoId="fAcT-LezxAY"
+            )
+            response = request.execute()
+            # loop through the commentThread request until no more pageToken too!
+            for commentThread in response.get("items", []):
+                top_level_comment = commentThread["snippet"]["topLevelComment"]
+                # Get the ID of the top_level_comment, then use youtube.comments.list() with its id!
+                replies = commentThread["replies"]["comments"]
+                print(f"Top Level Comment: {top_level_comment['snippet']['textDisplay']}")
+                print(f"Top Level Comment ID: {top_level_comment['id']}")
+                
+                # Loop through the replies....
+                replies_request = self.youtube_client.comments().list(
+                    part="snippet",
+                    maxResults=100,
+                    parentId=top_level_comment["id"]
+                )
+                replies_response = replies_request.execute()
+                num_replies = len(replies_response["items"])
+                # Number of comments under the top_level_comment
+                while next_page_token := replies_response.get("nextPageToken", None):
+                    replies_request = self.youtube_client.comments().list(
+                        part="snippet",
+                        maxResults=100,
+                        parentId=top_level_comment["id"],
+                        pageToken=next_page_token
+                    ) 
+                    # of course...gonna have to wrap the execute stuff with the try-catch statement    
+                    replies_response = replies_request.execute()
+                    num_replies += len(replies_response["items"])
+                
+                print(f"Number of comments extracted: {1+num_replies}")
+                
+                # TODO: make sure to output every single reply here! The one from the replies key only
+                # output a limited number of comments by default!
+                # print(f"The next page token is {nextPageToken}")
+                # nextPageToken = response.get("nextPageToken", None)
+        except HttpError as err:
+            print(err)
         return result
 
 
