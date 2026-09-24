b4412e5d4c26fa40cfb6161d8dc2bfd96003b28b
Huy Nguyen <dshk127mm@gmail.com>
2026-09-23 22:37:31 -0400

    Add some main model classes.

--- src/data/models.py
+++ src/data/models.py
@@ -1,4 +1,20 @@
 from sqlalchemy.orm import DeclarativeBase
 
+# Separate the models layer from the repositories to allow separation of responsibilities
+# The models describe what the data looks like, while the repositories describes how to perform operataions to get them.
 class Base(DeclarativeBase):
     pass
+
+class Videos(Base):
+    pass
+
+class Comments(Base):
+    pass
+
+class LiveComments(Base):
+    pass
+
+class Users(Base):
+    pass
+
+# TODO: Model the relationships too. We will also have to use junction tables to model some of the many-to-many relationships!
